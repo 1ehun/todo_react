eca2db7fa6eef874ce1dd1967a599ef6052c9432
1ehun <l3hunbot@gmail.com>
2023-03-10 23:55:49 +0300

    dropdown with delete

--- src/components/TodoDesk/index.jsx
+++ src/components/TodoDesk/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './index.css'
-import TodoField from '../TodoField';
 import {TASK_STATUSES} from '../TodoMain'
 /*js doc !!!*/ 
 
@@ -26,14 +25,73 @@ const TodoDesk = (props) => {
     const doneTasks = props.taskListProp.filter(item => 
     item.status === TASK_STATUSES.done
     )
+
+    const moveOnTodo = () => {
+        console.log('zaebis')
+    }
+
+    const moveOnProgress = () => {
+        console.log('zaebis')
+    }
+
+    const moveOnDone = () => {
+        console.log('zaebis')
+    }
+
+    const removeTask = (testitem) => {
+        console.log(testitem);
+    }
     
     return (
         <div className='todo-desk'>
-            <div>{todoTasks.map((item) => (
-                <div>{item.task}</div>
+            <div className='todo-desk-field'>
+                <h3>Todo tasks</h3>
+                <hr className='todo-desk-hr'/>
+                {todoTasks.map((item) => (
+                    <div className='todo-task'>
+                        {/* {item.status = 'todo'} */}
+                        <div style={{width: '100%'}}>
+                            <p style={{display: 'contents'}}>
+                                {/* {item.task} */}
+                            </p>
+                            <div className='dropdown'>
+                                <button className='drop-btn'>move</button>
+                                <div className='dropdown-content'>
+                                    <h6 
+                                    style={{cursor: "pointer"}}
+                                    onClick={moveOnTodo}
+                                    >On todo</h6>
+                                    <h6 
+                                    style={{cursor: "pointer"}}
+                                    onClick={moveOnProgress}
+                                    >On progress</h6>
+                                    <h6 
+                                    style={{cursor: "pointer"}}
+                                    onClick={moveOnDone}>On done</h6>
+                                     <h6 
+                                    style={{cursor: "pointer"}}
+                                    onClick={() => {
+                                        console.log(item.status)
+                                        item.status = 'inProgress'
+                                        console.log(item.status)
+                                    }}>Delete task</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}</div>
+            <div className='todo-desk-field'>
+                <h3>Tasks in progress</h3>
+                <hr className='todo-desk-hr'/>
+                {inProgressTasks.map((item) => (
+                    <div className='todo-task'>{item.task}</div>
             ))}</div>
-            <div>{inProgressTasks}</div>
-            <div>{doneTasks}</div>
+            <div className='todo-desk-field'>
+                <h3 className='todo-desk-line'>Done tasks</h3>
+                <hr className='todo-desk-hr'/>
+                {doneTasks.map((item) => (
+                    <div className='todo-task'>{item.task}</div>
+            ))}</div>
         </div>
     );
 }
